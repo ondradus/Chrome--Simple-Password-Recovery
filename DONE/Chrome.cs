9bc2667cc466967ca675c83e8f6f93604aba3ff7
Dondor17 <dusek3@spsejecna.cz>
2017-03-16 12:15:22 +0100

    Add files via upload
    I'm not responsible for anything you do with this.
    This is for educational purposes only.

---
+++ DONE/Chrome.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace SimpleChromepwrec
+{
+    public class Chrome
+    {
+        public static List<Field> GetSavedPasswords()
+        {
+            try
+            {
+                string datapath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                "Google\\Chrome\\User Data\\Default\\Login Data");
+                return ChromiumBase.Passwords(datapath, "Chrome");
+            }
+            catch (Exception)
+            {
+                return new List<Field>();
+            }
+        }
+
+        public static List<ChromiumBase.ChromiumCookie> GetSavedCookies()
+        {
+            try
+            {
+                string datapath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                "Google\\Chrome\\User Data\\Default\\Cookies");
+                return ChromiumBase.Cookies(datapath, "Chrome");
+            }
+            catch (Exception)
+            {
+                return new List<ChromiumBase.ChromiumCookie>();
+            }
+        }
+
+    }
+}
---
+++ DONE/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Runtime.InteropServices;
+using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace SimpleChromepwrec
+{
+    /*
+     <~> Simple module that shows locally stored passwords and cookies in Google Chrome
+     <~> No external DDLs or other dependencies needed. SQLiteHandler and other classes are already here :--)
+    
+         */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var kek in Chrome.GetSavedPasswords())
+            {
+                Console.WriteLine("SITE: " + kek.URL);
+                Console.WriteLine("USER: " + kek.Username);
+                Console.WriteLine("PASSWORD: " + kek.Password);
+                Console.WriteLine(Environment.NewLine);
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+
+   
+}
